b08d4d124f26d60009a388e6630adc973027db4c
Frederik <Frederik@10.50.131.105>
2015-04-09 12:39:06 +0200

    Merge origin/master
    Conflicts:
    	Dell_Projekt_Gruppe5/src/java/DataSourceLayer/DBConnector.java

--- Dell_Projekt_Gruppe5/src/java/Model/CampaignMapper.java
+++ Dell_Projekt_Gruppe5/src/java/Model/CampaignMapper.java
@@ -3,12 +3,46 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package DataSourceLayer;
+package Model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
  * @author Frederik
  */
 public class CampaignMapper {
+    
+    static boolean testRun = true;
+    
+    public boolean insertCampaign(ArrayList<Campaign> list, Connection con) throws SQLException{
+        int rowsInserted = 0;
+        String sqlString = "insert into kampagne values (?,?,?,?,?,?,?,?)";
+        PreparedStatement statement = null;
+        statement = con.prepareStatement(sqlString);
+        for (int i = 0; i < list.size(); i++) {
+            
+            Campaign c = list.get(i);
+            statement.setInt(1, list.get(i).getKno());
+            statement.setString(2, list.get(i).getBeskrivelse());
+            statement.setString(3, list.get(i).getStatus());
+            statement.setDate(4, list.get(i).getOprettelse_dato());
+            statement.setDate(5, list.get(i).getStart_dato());
+            statement.setDate(6, list.get(i).getSlut_dato());
+            statement.setInt(7, list.get(i).getPris());
+            statement.setInt(8, list.get(i).getPno());
+            rowsInserted += statement.executeUpdate();
+        }
+        if (testRun)
+        {
+            System.out.println("insertCampaign(): " + (rowsInserted == list.size())); // for test
+        }
+        statement.close();
+        return (rowsInserted == list.size());
+    }
+    
     
 }
