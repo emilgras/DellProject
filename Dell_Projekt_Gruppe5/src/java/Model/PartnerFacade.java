1fa38da9fec94a05fa807eab0577c1165d4c7279
AndersBjergfelt <bjergfelta@gmail.com$ git config user.email bjergfelta@gmail.com>
2015-04-09 13:45:21 +0200

    PartnerFacade

--- Dell_Projekt_Gruppe5/src/java/Model/PartnerFacade.java
+++ Dell_Projekt_Gruppe5/src/java/Model/PartnerFacade.java
@@ -12,9 +12,31 @@ import java.sql.Connection;
  * @author ABjergfelt
  */
 public class PartnerFacade {
-    
-    public void savePartner(Partner partner) {
-        
-    }
+     private PartnerMapper om; 
+	  private Connection con;
+	  private DBConnector dbcon;
+	  //== Singleton start
+	  private static PartnerFacade instance;
+	 
+	  private PartnerFacade() {
+		  om 	= new PartnerMapper();
+		  con 	= dbcon.getConnection();
+	  }
+	  public static PartnerFacade getInstance()
+	  {
+		  if(instance == null)
+			  instance = new PartnerFacade();
+		  return instance;
+	  }
+	  //== Singleton end
+          
+          public String getLogin(String username, String password){
+              return om.getLogin(username, password, con);
+              
+          }
+          
+          public String createPartner(Partner partner){
+              return om.createPartner(partner, con);
+          }
     
 }
