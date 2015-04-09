d4ef6b82c3c6330ac0188085577a75c6bc0a4c5a
Frederik <Frederik@10.50.131.105>
2015-04-09 12:39:18 +0200

    Merge origin/master
    Conflicts:
    	Dell_Projekt_Gruppe5/src/java/DataSourceLayer/DBConnector.java

---
+++ Dell_Projekt_Gruppe5/src/java/Model/Campaign.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.Date;
+/**
+ *
+ * @author Frederik
+ */
+public class Campaign {
+    int kno;
+    String beskrivelse;
+    String status;
+    Date oprettelse_dato;
+    Date start_dato;
+    Date slut_dato;
+    int pris;
+    int pno;
+
+    public Campaign(int kno, String beskrivelse, String status, Date oprettelse_dato, Date start_dato, Date slut_dato, int pris, int pno) {
+        this.kno = kno;
+        this.beskrivelse = beskrivelse;
+        this.status = status;
+        this.oprettelse_dato = oprettelse_dato;
+        this.start_dato = start_dato;
+        this.slut_dato = slut_dato;
+        this.pris = pris;
+        this.pno = pno;
+    }
+    
+    public Campaign(Campaign camp){
+        this.kno = camp.getKno();
+        this.beskrivelse = camp.getBeskrivelse();
+        this.status = camp.getStatus();
+        this.oprettelse_dato = camp.getOprettelse_dato();
+        this.start_dato = camp.getStart_dato();
+        this.slut_dato = camp.getSlut_dato();
+        this.pris = camp.getPris();
+        this.pno = camp.getPno();
+    }
+
+    public int getKno() {
+        return kno;
+    }
+
+    public void setKno(int kno) {
+        this.kno = kno;
+    }
+
+    public String getBeskrivelse() {
+        return beskrivelse;
+    }
+
+    public void setBeskrivelse(String beskrivelse) {
+        this.beskrivelse = beskrivelse;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getOprettelse_dato() {
+        return oprettelse_dato;
+    }
+
+    public void setOprettelse_dato(Date oprettelse_dato) {
+        this.oprettelse_dato = oprettelse_dato;
+    }
+
+    public Date getStart_dato() {
+        return start_dato;
+    }
+
+    public void setStart_dato(Date start_dato) {
+        this.start_dato = start_dato;
+    }
+
+    public Date getSlut_dato() {
+        return slut_dato;
+    }
+
+    public void setSlut_dato(Date slut_dato) {
+        this.slut_dato = slut_dato;
+    }
+
+    public int getPris() {
+        return pris;
+    }
+
+    public void setPris(int pris) {
+        this.pris = pris;
+    }
+
+    public int getPno() {
+        return pno;
+    }
+
+    public void setPno(int pno) {
+        this.pno = pno;
+    }
+    
+    
+    
+}
