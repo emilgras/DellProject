887d27fed1852fe51f6db0ccde04d9e4a8c78142
Frederik <Frederik@10.50.131.105>
2015-04-09 12:40:50 +0200

    Merge origin/master
    Conflicts:
    	Dell_Projekt_Gruppe5/src/java/DataSourceLayer/DBConnector.java

---
+++ Dell_Projekt_Gruppe5/src/java/Model/DBConnector.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author AndersBjergfelt
+ */
+public class DBConnector
+{
+
+    private static String driver = "oracle.jdbc.driver.OracleDriver";
+    private static String URL = "jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:dat";
+    private static String id = "cphfl66";			
+    private static String pw = "cphfl66";
+    
+    private Connection con;
+    
+    public DBConnector()
+    {
+        try
+        {
+            Class.forName(driver);
+            con = DriverManager.getConnection(URL, id, pw);   // The connection will be released upon program 
+		  					      // termination by the garbage collector	
+        } catch (Exception e)
+        {
+           
+            System.out.println("error in DBConnector.getConnection()");
+            System.out.println(e);
+        }
+    }
+    
+    
+    
+    public Connection getConnection()
+    {
+      return con;
+    }
+}
