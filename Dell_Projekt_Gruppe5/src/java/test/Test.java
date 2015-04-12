2a92b8dcc5d99549e2cb4acec23dce32e114ffdb
emilgras <emil.gras@gmail.com>
2015-04-12 16:34:57 +0200

    login + signup + newcamapgn Virker !

--- Dell_Projekt_Gruppe5/src/java/test/Test.java
+++ Dell_Projekt_Gruppe5/src/java/test/Test.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package test;
+package Test;
 import Model.Campaign;
 import Model.CampaignMapper;
 import Model.DBConnector;
