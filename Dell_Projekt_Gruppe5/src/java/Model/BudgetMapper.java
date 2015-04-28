64ed7daf8111cac13d167cdbc6919db1f088206c
emilgras <emil.gras@gmail.com>
2015-04-28 16:16:06 +0200

    nye changes

--- Dell_Projekt_Gruppe5/src/java/Model/BudgetMapper.java
+++ Dell_Projekt_Gruppe5/src/java/Model/BudgetMapper.java
@@ -17,38 +17,36 @@ import java.util.ArrayList;
  * @author Frederik
  */
 public class BudgetMapper {
+
     DBConnector db = new DBConnector();
-    
-    public ArrayList<Budget> getAllPrices(){
-       ArrayList<Budget> list = new ArrayList<>();
-        try(Connection connection = DriverManager.getConnection(db.getURL(), db.getId(),db.getPw())){
-            
-        
-        
-        String sql = "select kno, navn, pris from partner join kampagne on partner.pno = kampagne.pno";
-        PreparedStatement statement = null;
-        
+
+    public ArrayList<Budget> getAllPrices() {
+        ArrayList<Budget> list = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(db.getURL(), db.getId(), db.getPw())) {
+
+            String sql = "select kno, navn, pris from partner join kampagne on partner.pno = kampagne.pno";
+            PreparedStatement statement = null;
+
             statement = connection.prepareStatement(sql);
             ResultSet rs = statement.executeQuery();
-            while(rs.next()){
-                Budget b = new Budget(rs.getInt(1),rs.getString(2),rs.getFloat(3));
+            while (rs.next()) {
+                Budget b = new Budget(rs.getInt(1), rs.getString(2), rs.getFloat(3));
                 list.add(b);
             }
-       
+
         } catch (Exception e) {
             System.out.println("Fejl i getAllPrices()");
         }
         return list;
     }
-    
-    public int getStartsBelob(){
+
+    public int getStartsBelob() {
         int i = 0;
-        try(Connection connection = DriverManager.getConnection(db.getURL(), db.getId(),db.getPw())){
-        String sql = "select starts_belob from budget";
-        PreparedStatement statement = null;
-        ResultSet rs = null;
-        
-        
+        try (Connection connection = DriverManager.getConnection(db.getURL(), db.getId(), db.getPw())) {
+            String sql = "select starts_belob from budget";
+            PreparedStatement statement = null;
+            ResultSet rs = null;
+
             statement = connection.prepareStatement(sql);
             rs = statement.executeQuery();
             rs.next();
@@ -58,15 +56,14 @@ public class BudgetMapper {
         }
         return i;
     }
-    
-    public int getNuvaerendeBelob(){
+
+    public int getNuvaerendeBelob() {
         int i = 0;
-        try(Connection connection = DriverManager.getConnection(db.getURL(), db.getId(),db.getPw())){
-        String sql = "select nuvaernde_belob from budget";
-        PreparedStatement statement = null;
-        ResultSet rs = null;
-        
-     
+        try (Connection connection = DriverManager.getConnection(db.getURL(), db.getId(), db.getPw())) {
+            String sql = "select nuvaernde_belob from budget";
+            PreparedStatement statement = null;
+            ResultSet rs = null;
+
             statement = connection.prepareStatement(sql);
             rs = statement.executeQuery();
             rs.next();
@@ -76,18 +73,18 @@ public class BudgetMapper {
         }
         return i;
     }
-    
-    public boolean updateMoneyUsed(int i){
-         try(Connection connection = DriverManager.getConnection(db.getURL(), db.getId(),db.getPw())){
-        String sql = "update budget set nuvaernde_belob = " + (getNuvaerendeBelob() + i + " where starts_belob = " + getStartsBelob());
-        PreparedStatement statement = null;
-        
+
+    public boolean updateMoneyUsed(int i) {
+        try (Connection connection = DriverManager.getConnection(db.getURL(), db.getId(), db.getPw())) {
+            String sql = "update budget set nuvaernde_belob = " + (getNuvaerendeBelob() + i + " where starts_belob = " + getStartsBelob());
+            PreparedStatement statement = null;
+
             statement = connection.prepareStatement(sql);
             ResultSet rs = statement.executeQuery();
             rs.next();
-            
+
         } catch (Exception e) {
-             System.out.println("Fejl i updateMoneyUsed()");
+            System.out.println("Fejl i updateMoneyUsed()");
         }
         return true;
     }
